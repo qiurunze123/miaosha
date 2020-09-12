1608f1ad76f7f457b7a35f08f41f8cdaf36a2aa4
zhangchuan1210 <1004300749@qq.com>
2020-09-12 12:42:25 +0800

    修改目录结构

---
+++ tc/src/main/java/NettyServerHandler.java
@@ -0,0 +1,52 @@
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.channel.group.DefaultChannelGroup;
+import io.netty.util.concurrent.GlobalEventExecutor;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class NettyServerHandler extends ChannelInboundHandlerAdapter {
+
+     private static ChannelGroup channelGroup=new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
+     private static Map<String, List<String>> transactionIdMap=new HashMap();
+     @Override
+     public synchronized  void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+         System.out.println("接受数据："+msg.toString());
+         JSONObject jsonObject= JSON.parseObject((String) msg);
+         String command=jsonObject.getString("command");
+         String groupId=jsonObject.getString("groupId");
+         String transactionType=jsonObject.getString("transactionType");
+         String transactionId=jsonObject.getString("transactionId");
+         if("create".equals(command)){
+             transactionIdMap.put(groupId,new ArrayList<String>());
+         }else if("register".equals(command)){
+             transactionIdMap.get(groupId).add(transactionId);
+             if("rollback".equals(transactionType)){
+                 System.out.println("接收到回滚状态");
+                 sendMsg(groupId,"rollback");
+             }
+
+
+         }else if("cpmmit".equals(command)){
+             System.out.println("全局事务提交.");
+             sendMsg(groupId,"commit");
+         }else if("rollback".equals(command)){
+             System.out.println("全局事务回滚.");
+             sendMsg(groupId,"rollback");
+         }
+
+
+    }
+
+    private void sendMsg(String groupId, String commit) {
+    }
+
+
+}
