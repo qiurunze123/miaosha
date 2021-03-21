d5c0834b790686beb6370dabe335a130d4c8527e
zhangchuan1210 <1004300749@qq.com>
2021-03-21 15:12:00 +0800

    优化秒杀部分代码

---
+++ miaosha-2version/miaosha-common/src/main/java/com/geekq/miaosha/vo/GoodsDetailVo.java
@@ -0,0 +1,46 @@
+package com.geekq.miaosha.vo;
+
+import com.geekq.api.entity.GoodsVoOrder;
+import com.geekq.miaosha.entity.MiaoshaUser;
+
+public class GoodsDetailVo {
+    private GoodsVoOrder goods;
+
+    private int miaoshaStatus;
+
+    private MiaoshaUser user;
+
+    private int remainSeconds;
+
+    public void setGoods(GoodsVoOrder goods) {
+        this.goods = goods;
+    }
+
+    public GoodsVoOrder getGoods() {
+        return goods;
+    }
+
+    public void setMiaoshaStatus(int miaoshaStatus) {
+        this.miaoshaStatus = miaoshaStatus;
+    }
+
+    public int getMiaoshaStatus() {
+        return miaoshaStatus;
+    }
+
+    public void setUser(MiaoshaUser user) {
+        this.user = user;
+    }
+
+    public MiaoshaUser getUser() {
+        return user;
+    }
+
+    public void setRemainSeconds(int remainSeconds) {
+        this.remainSeconds = remainSeconds;
+    }
+
+    public int getRemainSeconds() {
+        return remainSeconds;
+    }
+}
