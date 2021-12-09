f2c2b929e30714aabb67ecc11db585de08c6c686
anran.war <anran.war@alibaba-inc.com>
2021-12-09 21:53:52 +0800

    删除无需跟踪文件

--- miaosha-admin/miaosha-admin-web/src/main/java/com/geekq/web/controller/LoginController.java
+++ miaosha-admin/miaosha-admin-web/src/main/java/com/geekq/web/controller/LoginController.java
@@ -5,25 +5,22 @@ import com.geekq.admin.service.ILogininfoService;
 import com.geekq.admin.service.RedisCacheStorageService;
 import com.geekq.common.enums.Constants;
 import com.geekq.common.utils.resultbean.ResultGeekQ;
-import com.geekq.common.vo.LoginVo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.validation.Valid;
 
 /**
  * @author 邱润泽
  * 登录模块
  */
 @Controller
-public class LoginController  extends BaseController {
+public class LoginController extends BaseController {
 
     private static Logger logger = LoggerFactory.getLogger(LoginController.class);
 
@@ -35,20 +32,19 @@ public class LoginController  extends BaseController {
     private RedisCacheStorageService redisCacheStorageService;
 
 
-
-    @RequestMapping("/login" )
+    @RequestMapping("/login")
     @ResponseBody
     public ResultGeekQ<Logininfo> dologin(HttpServletResponse response,
-                                        HttpServletRequest request,
-                                        String username, String password) {
+                                          HttpServletRequest request,
+                                          String username, String password) {
         ResultGeekQ<Logininfo> result = ResultGeekQ.build();
 
 
-        ResultGeekQ<Logininfo>  login = this.iLogininfoService.login(username, password,
-                Constants.USERTYPE_NORMAL,request.getRemoteAddr());
+        ResultGeekQ<Logininfo> login = this.iLogininfoService.login(username, password,
+                Constants.USERTYPE_NORMAL, request.getRemoteAddr());
         result.setData(login.getData());
-        if(!ResultGeekQ.isSuccess(login)){
-            result.withError(login.getCode(),login.getMessage());
+        if (!ResultGeekQ.isSuccess(login)) {
+            result.withError(login.getCode(), login.getMessage());
         }
         return result;
     }
